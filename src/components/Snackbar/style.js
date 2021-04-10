628d5ed3ad9b70ce4ff8b533381f13ee7b5bb3ad
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-10 20:52:58 +0530

    implemented styling in snackbar component

--- src/components/Snackbar/style.js
+++ src/components/Snackbar/style.js
@@ -1,29 +1,10 @@
-import React from 'react';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 
-import useStyles from './styles';
-
-const CustomizedSnackbar = ({ open, setOpen }) => {
-  const classes = useStyles();
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Snackbar 
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }} 
-      open={open} autoHideDuration={6000} onClose={handleClose}>
-        <MuiAlert onClose={handleClose} severity="success" elevation={6} variant="filled">Transaction successfully created.</MuiAlert>
-      </Snackbar>
-    </div>
-  );
-};
-
-export default CustomizedSnackbar;
+export default makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    },
+}));
