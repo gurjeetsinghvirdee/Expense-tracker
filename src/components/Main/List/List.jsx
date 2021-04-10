07e8bdfa0fd33ac5b09b1260d0b26d74716612d0
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-10 17:01:48 +0530

    added list component to main component

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List as MUILIST, ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { List as MUILIST, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
 
 import useStyles from './styles';
@@ -7,13 +7,27 @@ import useStyles from './styles';
 const List = () => {
     const classes = useStyles();
 
-    const transactions = [];
+    const transactions = [
+        { id: 1, type: "Income", category: 'Salary', amount: 80, date: new Date() }
+    ];
     
     return (
         <MUILIST desnse={false} className={classes.list}>
-            {transcations.map((transactions) => (
-                <Slide>
-                    
+            {transactions.map((transaction) => (
+                <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                                <MoneyOff />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText  primary={transaction.category} secondary={`${transaction.amount} - ${transaction.date}`} />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="delete" onClick="">
+                                <Delete />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
                 </Slide>
             ))}
         </MUILIST>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles';
 import Form from './Form/Form';
+import List from './List/List';
 
 function Main() {
     const classes = useStyles();
@@ -21,7 +22,7 @@ function Main() {
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* {List} */}
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
