081ea72e1b372bdb973aa9503b8458fef393ca44
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-10 17:40:02 +0530

    implementing a contextapi

--- src/context/context.js
+++ src/context/context.js
@@ -5,5 +5,9 @@ const initialState = [];
 export const ExpenseTrackercontext = createContext(initialState);
 
 export const Provider = ({ children }) => {
-    
+    return (
+        <ExpenseTrackercontext.Provider value={{ appname: `Expense Tracker`}}>
+            {children}
+        </ExpenseTrackercontext.Provider>
+    )
 }
