5847b5bed1355943d1a9f2c00885251240136d93
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-10 21:05:55 +0530

    implemented infocard with speeches

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const isIncome = Math.round(Math.random());
+
+const InfoCard = () => {
+    return (
+        <div elevation={3} style={{ textAlign: 'center', 'padding': '0 10%' }}>
+            Try saying: <br />
+            Add {isIncome ? 'Income ' : 'Expense '}
+            for {isIncome ? '$160 ' : '$80 '}
+            in Category {isIncome ? 'Salary ' : 'Travel '}
+            for {isIncome ? 'Monday ' : 'Friday '}
+        </div>
+    );
+};
+
+export default InfoCard;
